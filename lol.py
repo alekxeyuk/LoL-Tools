d6c55ae0930bd60d034406ea0e5c68af08ccc363
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-07-05 21:31:31 +1000

    Basic Building parse

--- lol.py
+++ lol.py
@@ -1,6 +1,16 @@
-# -*- coding: ISO-8859-1 -*-
-
+import math
 import base64
+
+
+'''# -*- coding: ISO-8859-1 -*-'''
+locales = {
+    'quality.0': "ужасного качества",
+    'quality.1': "низкого качества",
+    'quality.2': "среднего качества",
+    'quality.3': "хорошего качества",
+    'quality.4': "отличного качества",
+    'quality.5': "исключительного качества",
+}
 
 def pad0(n, length):
     line = '' + str(n)
@@ -28,3 +38,21 @@ class B64:
             result = result.replace('/', '~')
             result = result.replace('=', '_')
         return result
+
+
+class Building(object):
+    @staticmethod
+    def Building(data: str, sep: str = '|') -> dict:
+        params = ('id', 'type', 'level', 'status', 'name', 'quality', 'state', 'x', 'y', 'value', 'isIncompatible')
+        data_list = data.split(sep)
+        data_dict = dict(zip(params, data_list[:-3]))
+        data_dict.update({
+            'owner': dict(zip(('id', 'blazon', 'name'), data_list[-3:])),
+            'icon': f'{data_dict.get("type")}.{data_dict.get("level")}{data_dict.get("status")}'
+        })
+        del data_list, params
+        return data_dict
+
+    @staticmethod
+    def qualityAsString(quality: int) -> str:
+        return locales.get('quality.' + str(min(math.floor(float(quality) * 2.5), 5)))
