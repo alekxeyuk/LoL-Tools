8557564e50fa0ad881f78b96ba32d0b7fd300ecc
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-07-05 19:44:43 +1000

    Update XHR.py

--- XHR.py
+++ XHR.py
@@ -13,37 +13,43 @@ B64 = lol.B64()
 class XHR:
     """XHR requests class"""
  
-    def __init__(self):
+    def __init__(self, token: str, phpsessid: str, lang: str = 'ru'):
         """Constructor"""
-        self.token = 'f8bGW6mqpb2wlTwGmTK7'
+        self.token = token
+        self.session = requests.Session()
+        self.session.cookies.update({'lang': lang, 'PHPSESSID': phpsessid})
 
     def token_update(self, url = None):
         pass
 
     def query(self, query, args):
-        r = requests.post("https://www.landsoflords.com/ajax/query", data={'token': self.token, 'q': self.encode(query, args)}, cookies={'lang': 'ru', 'PHPSESSID': 'eugs5lct2pvkepuhelpoll5ie1'})
+        r = self.session.post("https://www.landsoflords.com/ajax/query", data={'token': self.token, 'q': self.encode(query, args)})
         if r.text == "" or r.text[0] == "!":
             return "!eRRor: " + r.text
         return self.decode(r.text)
 
-    def decode(self, data):
+    def _decode(self, data: str, kstring_arg: int = 3) -> str:
         data = data.replace('\n\r', '')
         data = data.strip()
         data = B64.decode(data).decode('ISO-8859-1')
         ts = int(data[0:4])
         kstring = self.token + str(ts)
         klen = len(kstring)
-        k = ''
-        for i in range(0, 20):
-            k += kstring[int((ts + i * 3) % klen):int((ts + i * 3) % klen + 1)]
+        k = ''.join([kstring[(ts + i * kstring_arg) % klen] for i in range(0, 20)])
         result = ''
         data = data[4:]
         for i in range(len(data)):
             c = ord(data[i])
             kc = ord(k[(i * 7) % len(k)])
             result += chr((c - kc) % 256)
-        
+
         return decodeURIComponent(result)
+
+    def decode_client(self, data: str) -> str:
+        return self._decode(data, 7)
+
+    def decode_server(self, data: str) -> str:
+        return self._decode(data, 3)
 
     def encode(self, query, args):
         q = ''
@@ -64,20 +70,21 @@ class XHR:
             enc += chr((c + kc) % 256)
         return B64.encode(enc)
 
-XHR = XHR()
+if __name__ == "__main__":
+    XHR = XHR(input('Enter token'), input('Enter PHPSESSID'))
 
-#for x in range(-476, -800, -1):
-data = XHR.query('map', {
-            'nav': 0,
-            'x': 0,
-            'y': 0,
-            'w': 10000,
-            'h': 10000,
-            'zoom': 1,
-            'mode': 'st'
-        })
+    #for x in range(-476, -800, -1):
+    data = XHR.query('map', {
+        'nav': 0,
+        'x': 0,
+        'y': 0,
+        'w': 20000,
+        'h': 20000,
+        'zoom': 1,
+        'mode': 'st'
+    })
 
-if data[0] != "!":
+    if data[0] != "!":
         mapdecode.updateWithData(data)
-else:
+    else:
         print(data)
